f61fe7c31f15760777b57d571c427b1b13553233
Kratik Jarwal <alpha101ml@gmail.com>
2026-06-03 01:50:34 +0530

    Add files via upload

---
+++ Day 1 – Your first Python program/hello.py
@@ -0,0 +1,16 @@
+print("hello, world!")
+
+print("2 + 3 =", 2 + 3)
+
+# 2 + 3 is a calculation. The computer does the math, and print shows the result.
+
+my_name = "Kratik Jarwal"
+age = 20
+print("My name is", my_name)
+print("I am", age, "years old")
+
+# my_name = "Your Name" creates a variable (a labeled box) holding text.
+
+# age = 25 holds a number.
+
+# Then we print them.
